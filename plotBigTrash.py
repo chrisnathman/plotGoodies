8b272cfff01bab495b1217369e29b6ce94fefb08
chrisnathman <nathmanc@gmail.com>
2019-10-26 01:16:04 -0700

    added file to generate and plot graphs

---
+++ plotBigTrash.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import math as math
+
+def generate_data():
+    xData = []
+    yData = []
+    for i in range(0, 100):
+        xData.append(i * 0.1)
+        yData.append(math.sin(i * 0.1))
+    data = [xData, yData]
+    return data
+
+
+if __name__ == '__main__':
+    data = generate_data()
+    plt.plot(data[0], data[1])
+    plt.grid()
+    plt.show()
