ace1faa4848ff4765c4b33504eb881c48a9e2d3b
chrisnathman <nathmanc@gmail.com>
2019-10-29 00:32:41 -0700

    added comments and some transforms

--- plotBigTrash.py
+++ plotBigTrash.py
@@ -12,8 +12,23 @@ def generate_data():
     return data
 
 
+# amplitude of sine waves from fft is magnitude of complex vector
+# phase of sine waves from fft is angle of complex vector
+# divide amplitude by number of samples to get actual amplitude
+# ignore all fft values above half of sampling frequency and increase amplitudes
+# of remaining fft values accordingly, ie if half of fft values removed, double
+# remaining amplitudes
+# phase is given in terms of cosine terms
 if __name__ == '__main__':
     data = generate_data()
-    plt.plot(data[0], fft.fft(data[1]))
+
+    # trying a bunch of different transforms
+    fftData = fft.fft(data[1])
+    rfftData = fft.rfft(data[1])
+    hfftData = fft.hfft(data[1])
+   # fftFrequencies = fft.fftfreq(data[1])
+
+    plt.plot(data[1])
+
     plt.grid()
     plt.show()
